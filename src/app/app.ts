aefcc4bbb32298dbf7579d29117af1753db4ac87
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2026-01-16 12:46:38 -0400

    preparando la aplicacion

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormularioPersonas } from "./personas/formulario-personas/formulario-personas";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormularioPersonas],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/categorias/categorias.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categorias',
+  imports: [],
+  templateUrl: './categorias.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Categorias { }
