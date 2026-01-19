c71b59ad41ff081af521faf315302fe370a9fb1d
Ramon Santos <82469695+ramonantoniosantoscuevas15@users.noreply.github.com>
2026-01-19 12:53:53 -0400

    preparando los componentes

--- src/app/formulario-personas/correos/correos.ts
+++ src/app/formulario-personas/correos/correos.ts
@@ -1,23 +1,58 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { ReactiveFormsModule, FormsModule, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, forwardRef, inject } from '@angular/core';
+import { ReactiveFormsModule, FormsModule, FormBuilder, Validators, FormArray, FormControl, ControlValueAccessor, Validator, AbstractControl, ValidationErrors, NG_VALUE_ACCESSOR, NG_VALIDATORS } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormUtilidades } from '../../utils/form-utilidades';
 import { MatButtonModule } from '@angular/material/button';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-correos',
   imports: [ReactiveFormsModule, MatFormFieldModule, FormsModule, MatInputModule,MatButtonModule],
   templateUrl: './correos.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers:[
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting:forwardRef(()=> Correos),
+      multi:true
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(()=>Correos),
+      multi:true
+    }
+  ]
 })
-export class Correos {
+export class Correos implements ControlValueAccessor,Validator {
+ validate(control: AbstractControl): ValidationErrors | null {
+    return this.form.valid ? null: {invalidEmails: true}
+  }
+
+  private sub?: Subscription
+  onTouchedCb?: ()=> void
+  writeValue(obj: any): void {
+    obj && this.form.setValue(obj,{emitEvent: false})
+  }
+  registerOnChange(fn: any): void {
+   this.sub = this.form.valueChanges.subscribe(fn)
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouchedCb = fn
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    isDisabled ? this.form.disable() : this.form.enable()
+  }
+  ngOnDestroy():void{
+    this.sub?.unsubscribe()
+  }
    private fb = inject(FormBuilder)
   formUtilidades = FormUtilidades
 
   form = this.fb.group({
+    correo:['',[Validators.required,Validators.pattern(this.formUtilidades.emailPattern)]],
     correos:this.fb.array([
-      ['',[Validators.required,Validators.pattern(this.formUtilidades.emailPattern)]],
+      [''],
     ])
   })
 
@@ -31,7 +66,15 @@ export class Correos {
   }
 
   newcorreo(){
+    if(this.correos.invalid) return
     this.correos.push(this.fb.control('',[Validators.required,Validators.pattern(this.formUtilidades.emailPattern)]))
+  }
+
+  agregarcorreo(){
+    if (!this.form.valid) {
+
+      return
+    }
   }
 }
 
